e4f0a0718c0d1deb628c0c931b7e12ccae90385e
nickguerra <nickg@maliciousgames.com>
2021-10-14 07:25:55 -0500

    Added Controller for Item List

---
+++ InAndOut/Controllers/ItemController.cs
@@ -0,0 +1,29 @@
+﻿using InAndOut.Data;
+using InAndOut.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InAndOut.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+
+        public ItemController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Item> objList = _db.Items;
+
+            return View(objList);
+        }
+
+    }
+}
